682a74a8b79874b3106a099574e60e12ba3e597a
PinedaAndy <63311777+PinedaAndy@users.noreply.github.com>
2021-01-14 13:22:44 -0500

    Created school pages each with their own unique static paths. The folder can be found under pages.I made the page id the school name to make the url a bit more attractive and clean. In order to not generate an error i rreplaced all white spaces within the school name with a dash '-'. For example University Of Maryland becomes University-Of-Maryland. I then loaded the school data through getStaticProps feature of Nextjs. We then have acces to all the school information from the database. To complete both these tasks a new functions folder was created under lib/ called lib/school. In there are two new functions. One getAllSchoolId's to get all the school names to use for the static path id. Then we have getSchoolData with the parameter school. I then get all the information pertaining to that spefic school

---
+++ pages/schools/[id].js
@@ -0,0 +1,59 @@
+import getSchoolIds from '../../lib/school/getAllSchoolIds'
+import getSchoolInfo from '../../lib/school/getSchoolData'
+
+
+
+
+
+
+
+
+export default function Schools({school}){
+
+
+
+    return(
+
+        <>  
+
+            <h2>Welcome To {school.name} Page</h2>
+            <h3>{school.nickname}</h3>
+
+        
+        </>
+
+
+    )
+}
+
+
+
+
+export async function getStaticPaths() {
+
+    //Get all paths by school name
+    const paths = await getSchoolIds() 
+    console.log(paths)
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+
+export async function getStaticProps({ params }) {
+
+    //Remove dashes from school url name and turn it to original format 
+    const school_name = params.id.split('-').join(' ')
+    console.log(school_name)
+
+    // Gets the school information for the current school 
+    const school = await getSchoolInfo(school_name)
+    
+
+    return {
+        props: {school}, // will be passed to the page component as props
+    }
+}
+
